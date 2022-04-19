b637a6636a6f1e06d71df478824a59243aa4179b
Алексей Панников <a.pannikov@tirscript.com>
2022-04-19 16:03:25 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-require('dotenv').config()
+// require('dotenv').config()
 
 // https://vitejs.dev/config/
 
 export default defineConfig(({ command, mode }) => {
-  console.log('base-irl', process.env.BASE_URL);
+  // console.log('base-irl', process.env.BASE_URL);
   
   if (command === 'serve') {    
     return {
@@ -15,7 +15,7 @@ export default defineConfig(({ command, mode }) => {
     // command === 'build'
     return {
       plugins: [vue()],
-      base: process.env.BASE_URL
+      // base: process.env.BASE_URL
     }
   }
 })
