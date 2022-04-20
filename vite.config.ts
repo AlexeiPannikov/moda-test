afedc255cdce22a7d3e141aa7595cd629de73ab2
Алексей Панников <a.pannikov@tirscript.com>
2022-04-20 13:13:57 +0300

    настройки проекта

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,34 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 // require('dotenv').config()
 
 // https://vitejs.dev/config/
+console.log(process.env);
 
-export default defineConfig(({ command, mode }) => {
-  // console.log('base-irl', process.env.BASE_URL);
-  
-  if (command === 'serve') {    
-    return {
-      plugins: [vue()]
-    }
-  } else {
-    // command === 'build'
-    return {
-      plugins: [vue()],
-      // base: process.env.BASE_URL
-    }
+
+
+export default defineConfig({  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@functions': path.resolve(__dirname, './src/functions'),
+      '@layout': path.resolve(__dirname, './src/components/layout'),
+      '@pages': path.resolve(__dirname, './src/components/pages'),
+    },
+  },
+
+  plugins: [vue()],
+
+  build: {
+    outDir: 'dist'
+  },
+
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    hmr: true
   }
 })
